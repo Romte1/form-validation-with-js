8067552d5feb5dda570e221d5aa97b6c94b973ac
Romte1 <ivan_ni224@hotmail.com>
2025-01-06 21:02:42 -0300

    More changes made

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const email = document.getElementById("mail");
 
 email.addEventListener("input", (event) => {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("I am expecting an email address!");
-  } else {
-    email.setCustomValidity("");
+  // Validate with the built-in constraints
+  email.setCustomValidity("");
+  if (!email.validity.valid) {
+    return;
+  }
+
+  // Extend with a custom constraints
+  if (!email.value.endsWith("@example.com")) {
+    email.setCustomValidity("Please enter an email address of @example.com");
   }
 });
